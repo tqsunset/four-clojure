ecb35dade011c238f2d9cb5df419b214a73047ab
tbsnfruits <llinoa22@gmail.com>
2020-12-06 23:29:28 +0900

    Comparisions Product Digits Group a Sequence Dot product Read a binary number

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -9,3 +9,35 @@
 
 (defn iterate* [f init]
   (cons init (lazy-seq (iterate* f (f init)))))
+
+;;Comparisions
+(defn compare* [op d1 d2]
+  (let [d1<d2 (op d1 d2)
+        d1>d2 (op d2 d1)]
+    (cond
+      (not (or d1<d2 d1>d2)) :eq
+      d1<d2 :lt
+      :else :gt)))
+
+;;Product Digits
+(defn prod-digit [a b]
+  (->> (* a b)
+      str
+      seq
+      (map #(- (int %) (int \0)))))
+
+;;Group a Sequence
+(defn group-by* [pred coll]
+  (reduce (fn [acc x]
+            (update acc
+                    (pred x)
+                    #(vec (conj % x))))
+          {} coll))
+
+;;Dot product
+(defn dot-product [coll1 coll2]
+  (apply + (map * coll1 coll2)))
+
+;;Read a binary number
+(defn read-binary [string]
+  (Integer/parseInt string 2))
